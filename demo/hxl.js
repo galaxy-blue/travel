bb71bee0e7c6ad836df56d74d3c6374c9462d0d1
galaxy-blue <16631116365@163.com>
2019-04-25 15:35:26 +0800

    'vue'

---
+++ demo/hxl.js
@@ -0,0 +1,39 @@
+var class_json = {
+  zp:{
+    province_id : {
+      temp: 0,
+      beijing: 1,
+      shanghai: 1,
+      tianjin: 1,
+      chongqing: 1,
+      hebei: 1,
+      shanxi: 1,
+      neimenggu: 1,
+      liaoning: 1,
+      jilin: 1,
+      heilongjiang: 1,
+      jiangsu: 1,
+      zhejiang: 1,
+      anhui: 15,
+      fujian: 16,
+      jiangxi: 17,
+      shandong: 12,
+      henan: 18,
+      hubei: 18,
+      hunan: 19,
+      guangdong: 20,
+      guangxi: 21,
+      hainan: 22,
+      sichuan: 23,
+      guizhou: 24,
+      yunnan: 25,
+      xizang: 26,
+      sx: 27,
+      gansu: 28,
+      qinghai: 29,
+      ningxia: 30,
+      xinjiang: 31,
+      shenzhen: 101
+    }
+  }
+};
